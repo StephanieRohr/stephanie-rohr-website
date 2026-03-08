5e3f8cdca4e38fae0579b9e5fce74eb9e25a2c60
phillip-hirsch <phillip@350home.com>
2026-03-08 19:09:47 -0400

    chore: add the Netlify adapter for Astro
    Signed-off-by: phillip-hirsch <phillip@350home.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 
+import netlify from '@astrojs/netlify';
 import react from '@astrojs/react'
+
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  adapter: netlify(),
 })
