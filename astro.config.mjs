c546e13f97c8ef9a1edf1535e590f6c3f62d7b00
phillip-hirsch <phillip@350home.com>
2026-03-23 19:47:03 -0400

    feat: Add Raleway font integration with Astro Font API and update global styles
    Signed-off-by: phillip-hirsch <phillip@350home.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import netlify from '@astrojs/netlify'
 import react from '@astrojs/react'
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   adapter: netlify(),
+  fonts: [
+    {
+      name: 'Raleway',
+      cssVariable: '--font-heading',
+      provider: fontProviders.google(),
+    },
+  ],
 })
